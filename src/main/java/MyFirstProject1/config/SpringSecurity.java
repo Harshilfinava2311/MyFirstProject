c07aab0982335b2b1df8739f88661d7cbc877537
Harshil <harshilfinava321@gmail.com>
2026-02-13 07:48:29 +0530

    new

---
+++ src/main/java/MyFirstProject1/config/SpringSecurity.java
@@ -0,0 +1,14 @@
+package MyFirstProject1.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.*;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurity extends WebSecurityConfiguration {
+//
+//
+//
+//}
