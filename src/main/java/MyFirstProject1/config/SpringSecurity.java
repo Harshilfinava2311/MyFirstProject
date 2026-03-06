20389122025c4b9e1c8b897d56061bdae68a78f1
Harshil <harshilfinava321@gmail.com>
2026-03-06 22:46:51 +0530

    security

--- src/main/java/MyFirstProject1/config/SpringSecurity.java
+++ src/main/java/MyFirstProject1/config/SpringSecurity.java
@@ -1,8 +1,11 @@
 package MyFirstProject1.config;
 
+import MyFirstProject1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +16,10 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
+
+    @Autowired
+    private UserService userService;
+
     @Bean
     public SecurityFilterChain securityfilterchain(HttpSecurity http) throws Exception {
         http
@@ -27,6 +34,11 @@ public class SpringSecurity {
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
